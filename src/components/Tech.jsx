c94cc2a737b3871d285e6508e39bd96b03713f94
Vivek <vivekthakur6259@gmail.com>
2023-09-09 23:46:15 +0530

    :fire: canvas problem solved

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
+import { motion } from "framer-motion";  
+import { styles } from "../styles";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Tech = () => {
+
+  const icons = Object.values(technologies).map((technology) => technology.icon);
+
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+      <p className={`${styles.sectionSubText} `}>My skills</p>
+          <h2 className={`${styles.sectionHeadText}`}>
+        TECHNOLOGIES.
+        </h2>
+      </motion.div>
+      <div className='w-full h-[75vh] mx-auto'>
+            <BallCanvas icons={icons} />
+      </div>
+    </>
   );
 };
 
