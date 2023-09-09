7b1b883701668dc10d785e08316bd3511110e544
Vivek <vivekthakur6259@gmail.com>
2023-09-09 16:43:59 +0530

    Production Build

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
-import { motion } from "framer-motion";
-
+import { motion } from "framer-motion";  
+ 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
-const Hero = () => {
+const Hero = () => { 
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
